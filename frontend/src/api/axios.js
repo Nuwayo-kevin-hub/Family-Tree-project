b077a69aaa354cfdbd390505924108203125e305
Nuwayo-kevin-hub <nuwayokevin@gmail.com>
2026-08-20 02:58:18 -0700

    Connect frontend to backend

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,48 +1,23 @@
 import axios from "axios";
 
-
 const api = axios.create({
-
-    baseURL:"http://localhost:5000/api",
-
-    headers:{
-        "Content-Type":"application/json"
+    baseURL: `${import.meta.env.VITE_API_URL}/api`,
+    headers: {
+        "Content-Type": "application/json"
     }
-
 });
 
-
-
-
-
 api.interceptors.request.use(
+    (config) => {
+        const token = localStorage.getItem("token");
 
-(config)=>{
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
 
-
-const token = localStorage.getItem("token");
-
-
-if(token){
-
-    config.headers.Authorization =
-    `Bearer ${token}`;
-
-}
-
-
-
-return config;
-
-
-},
-
-(error)=>Promise.reject(error)
-
-
+        return config;
+    },
+    (error) => Promise.reject(error)
 );
-
-
-
 
 export default api;
